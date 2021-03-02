66eadfab540603780c4ae46ae8125848257aa749
03PatriciaG <patricia.garcia.almanza@gmail.com>
2021-03-02 15:50:04 -0600

    pagina

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, PrecioPageView, AboutPageView
 
 urlpatterns = [
     path('about', AboutPageView.as_view(), name = 'about'),
-    #path('about', AboutPageView.as_view(), name = 'about')
-    path('', HomePageView.as_view(), name = 'home')
+    path('', HomePageView.as_view(), name = 'home'),
+    path('precio', PrecioPageView.as_view(), name = 'precio')
+
+
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,11 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+class PrecioPageView(TemplateView):
+    template_name = 'precio.html'
+
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
 
 # Create your views here.
